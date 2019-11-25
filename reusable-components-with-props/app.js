91b6c5a5df5d3337827b7dffd8325eafd92d6c1d
andrelucasmartins <andrelucasmartins@hotmail.com>
2019-11-24 23:26:51 -0300

    Communication Between Components with Custom Events

--- reusable-components-with-props/app.js
+++ reusable-components-with-props/app.js
@@ -1,8 +1,48 @@
-Vue.component('plan', {
+let PlanComponent = {
     template: '#plan-template',
-    props: ['name']
-})
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        selectedPlan: {
+            type: String
+        }
+    },
+    computed: {
+        isSelected(){
+           return this.name === this.selectedPlan 
+        }
+    },
+    methods: {
+        select(){
+            this.$emit('select', this.name)
+        }
+    }
+}
+
+let planPickerComponent = {
+    template: "#plan-picker-template",
+    components: {
+        plan: PlanComponent
+    },
+    data(){
+        return{
+            plans: ['The Single', 'The Curious', 'The Addict'],
+            selectedPlan: null
+        }
+    },
+    methods: {
+        selectPlan(plan){
+            this.selectedPlan = plan
+        }
+    }
+}
+
 
 new Vue({
-    el: "#app"
+    el: '#app',
+    components: {
+        'plan-picker': planPickerComponent
+    }
 })
